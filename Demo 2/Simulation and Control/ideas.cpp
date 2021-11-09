4f684560677808cdc931a6ab197a0a3a26e9b7ff
350-helper <91347870+350-helper@users.noreply.github.com>
2021-11-08 20:16:07 -0700

    Update ideas.cpp

--- Demo 2/Simulation and Control/ideas.cpp
+++ Demo 2/Simulation and Control/ideas.cpp
@@ -7,7 +7,7 @@ void maneuvering ()
   switch(maneuveringState)
     
     case SEARCH_TAPE:
-    if (receivedCVInstructions != 0)
+    if (transmittedCVInstructions != 0)
     {
       maneuveringState = CALIBRATION;
       // Initialize othe parameters
