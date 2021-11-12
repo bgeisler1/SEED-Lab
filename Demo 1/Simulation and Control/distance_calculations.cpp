f8c49c0bb92829ff2cc6906ae79e8d79c631361c
350-helper <91347870+350-helper@users.noreply.github.com>
2021-11-12 00:47:49 -0700

    Create distance_calculations.cpp

---
+++ Demo 1/Simulation and Control/distance_calculations.cpp
@@ -0,0 +1,26 @@
+# define constants
+ALPHA  (17/180) * PI
+H 0.17
+
+loop()
+{
+ *********************************
+ /* Stuff from the previous codes
+ *********************************
+ 
+ */
+  
+thetaRadians = theta/180 * PI;
+phiRadians = phi/180 * PI;
+totalRho = H * (tan(ALPHA) + tan(PI/2 - thetaRadians + ALPHA));
+
+// If sensed a change, update the desiredValues in the functions
+if (theta != storedTheta || phi != storedPhi)
+{
+desiredRho = totalRho;
+desiredPhi = phiRadians;
+}
+
+storedTheta = theta;
+storedPhi = phi;
+}
